6ed1be0e674d20007b84ea79642724f292bd4005
a-holm <jaholm.consulting@gmail.com>
2017-08-25 18:17:19 +0200

    add test and training data + confidence and accuracy values

--- kNearestNeighbors/howItWorksKNearestNeighbors.py
+++ kNearestNeighbors/howItWorksKNearestNeighbors.py
@@ -20,19 +20,14 @@ Todo:
 """
 from collections import Counter
 import numpy as np
-# import matplotlib.pyplot as plt
-from matplotlib import style
-# from math import sqrt
+import pandas as pd
+import random
 import warnings
-style.use('fivethirtyeight')
+
 
 # hardcoded testdata
 dataset = {'k': [[1, 2], [2, 3], [3, 1]], 'r': [[6, 5], [7, 7], [8, 6]]}
 new_features = [5, 7]
-
-# [[plt.scatter(ii[0], ii[1], s=100, color=i) for ii in dataset[i]] for i in dataset]
-# plt.scatter(new_features[0], new_features[1], s=100)
-# plt.show()
 
 
 def k_nearest_neighbors(data, predict, k=3):
@@ -42,6 +37,8 @@ def k_nearest_neighbors(data, predict, k=3):
     of the training data and their label. In a larger dataset it would make
     sense to specify a radius to avoid going over all data points each time,
     but with the current dataset it does not matter so I avoid it to simplify.
+
+    Can work on both linear and non-linear data sets.
 
     Args:
         data (dictionary): a dictionary where the keys are labels and the
@@ -55,19 +52,54 @@ def k_nearest_neighbors(data, predict, k=3):
 
 
     """
+    predict = np.array(predict)
     if len(data) >= k:
         warnings.warn('K is set to a value less than total voting groups')
     distances = []
     for group in data:
         for features in data[group]:
-            # euclidean_distance = np.sqrt(np.sum((np.array(features)-np.array(predict))**2))
-            euclidean_distance = np.linalg.norm(np.array(features) - np.array(predict))  # faster
+            features = np.array(features)
+            # euclidean_distance = np.sqrt(np.sum(features-predict)**2))
+            euclidean_distance = np.linalg.norm(features - predict)  # faster
             distances.append([euclidean_distance, group])
 
     votes = [i[1] for i in sorted(distances)[:k]]
-    print(Counter(votes).most_common(1))
     vote_result = Counter(votes).most_common(1)[0][0]
-    return vote_result
+    confidence = Counter(votes).most_common(1)[0][1] / k
 
-result = k_nearest_neighbors(dataset, new_features, k=5)
-print(result)
+    return vote_result, confidence
+
+# Setup data
+df = pd.read_csv('breast-cancer-wisconsin.data')
+df.replace('?', -99999, inplace=True)
+df.drop(['id'], 1, inplace=True)
+full_data = df.astype(float).values.tolist()
+random.shuffle(full_data)
+
+# make test and training data
+test_size = 0.2
+train_set = {2: [], 4: []}
+test_set = {2: [], 4: []}
+train_data = full_data[:-int(test_size * len(full_data))]  # first 80%
+test_data = full_data[-int(test_size * len(full_data)):]  # last 20%
+
+# populate test_set and train_set
+for i in train_data:
+    train_set[i[-1]].append(i[:-1])
+for i in test_data:
+    test_set[i[-1]].append(i[:-1])
+
+
+# test Accuracy
+correct = 0
+total = 0
+
+for group in test_set:
+    for data in test_set[group]:
+        vote, conf = k_nearest_neighbors(train_set, data, k=5)
+        if group == vote:
+            correct += 1
+        else:
+            print(conf)
+        total += 1
+print('Accuracy: ', correct / total)
