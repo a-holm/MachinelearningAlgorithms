740fe77bbcfef0cebfceece528dbb66937a5a75a
a-holm <jaholm.consulting@gmail.com>
2017-08-26 20:10:19 +0200

    Add functions to create testing and training data

--- softMarginSVMwithKernels/howItWorksSoftMarginSVM.py
+++ softMarginSVMwithKernels/howItWorksSoftMarginSVM.py
@@ -163,3 +163,66 @@ class SVM(object):
             """Method to predict features X."""
             return np.sign(self.project(X))
 
+if __name__ == '__main__':
+
+    def gen_lin_seperable_data():
+        """Function to generate linearly seperable 2d training data."""
+        mean1 = np.array([0, 2])
+        mean2 = np.array([2, 0])
+        cov = np.array([[0.8, 0.6], [0.6, 0.8]])
+        X1 = np.random.multivariate_normal(mean1, cov, 100)
+        y1 = np.ones(len(X1))
+        X2 = np.random.multivariate_normal(mean2, cov, 100)
+        y2 = np.ones(len(X2)) * -1
+        return X1, y1, X2, y2
+
+    def gen_non_lin_seperable_data():
+        """Function to generate non-linear seperable training data."""
+        mean1 = [-1, 2]
+        mean2 = [1, -1]
+        mean3 = [4, -4]
+        mean4 = [-4, 4]
+        cov = np.array([[1.0, 0.8], [0.8, 1.0]])
+        X1 = np.random.multivariate_normal(mean1, cov, 50)
+        X1 = np.vstack(np.random.multivariate_normal(mean3, cov, 50))
+        y1 = np.ones(len(X1))
+        X2 = np.random.multivariate_normal(mean2, cov, 50)
+        X2 = np.vstack(np.random.multivariate_normal(mean4, cov, 50))
+        y2 = np.ones(len(X2)) * -1
+        return X1, y1, X2, y2
+
+    def gen_lin_seperable_overlap_data():
+        """Function to generate linearly seperable overlapping data.
+
+        Function to generate linearly seperable, but overlapping 2d training
+        data. This is training data that needs a soft margin
+        """
+        mean1 = np.array([0, 2])
+        mean2 = np.array([2, 0])
+        cov = np.array([[1.5, 1.0], [1.0, 1.5]])
+        X1 = np.random.multivariate_normal(mean1, cov, 100)
+        y1 = np.ones(len(X1))
+        X2 = np.random.multivariate_normal(mean2, cov, 100)
+        y2 = np.ones(len(X2)) * -1
+        return X1, y1, X2, y2
+
+    def split_train(X1, y1, X2, y2):
+        """Function to split the training data."""
+        X1_train = X1[:90]
+        y1_train = y1[:90]
+        X2_train = X2[:90]
+        y2_train = y2[:90]
+        X_train = np.vstack((X1_train, X2_train))
+        y_train = np.vstack((y1_train, y2_train))
+        return X_train, y_train
+
+    def split_test(X1, y1, X2, y2):
+        """Function to split the testing data."""
+        X1_test = X1[90:]
+        y1_test = y1[90:]
+        X2_test = X2[90:]
+        y2_test = y2[90:]
+        X_test = np.vstack((X1_test, X2_test))
+        y_test = np.vstack((y1_test, y2_test))
+        return X_test, y_test
+
