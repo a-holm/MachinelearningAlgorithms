ea60286d4142a23d082460617d020ea6949e9bab
a-holm <jaholm.consulting@gmail.com>
2017-08-26 21:53:45 +0200

    Add skeleton files for kMeans

---
+++ kMeans/howItWorksKMeans.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""K-Means unsupervised classification for machine learning.
+
+K-means clustering is a unsupervised method to cluser or group the data.
+K-means allows you to choose the number (k) of categories/groups and
+categorizes it automatically when it has come up with solid categories.
+
+Example:
+
+        $ python howItWorksKMeans.py
+
+Todo:
+    *
+"""
