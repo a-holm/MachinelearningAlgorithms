d67697470067ed9e6bf0bc93059767580f5c858a
a-holm <jaholm.consulting@gmail.com>
2017-08-27 21:45:38 +0200

    Add deep learning project with custom data

---
+++ deepLearningWithNeuralNetworks/ownDataDeepLearningWithNeuralNetworks.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""Deep Learning with Neural Networks and TensorFlow.
+
+Deep learning is part of a broader family of machine learning methods based on
+learning data representations, as opposed to task-specific algorithms. Learning
+can be supervised, partially supervised or unsupervised.
+
+A deep neural network (DNN) is an artificial neural network with multiple
+hidden layers between the input and output layers.
+
+We will use the library Tensorflow to do number crunching. A package like
+TensorFlow allows us to perform specific machine learning number-crunching
+operations tensors with large efficiency. We can also easily distribute this
+processing across our CPU cores, GPU cores, or even multiple devices like
+multiple GPUs. But that's not all! We can even distribute computations across
+a distributed network of computers with TensorFlow.
+
+The data used is from the MNIST database of handwritten digits:
+http://yann.lecun.com/exdb/mnist/ but is imported by tensorflow library
+
+Example:
+
+        $ python regularDeepLearningWithNeuralNetworks.py
+
+Todo:
+    *
+"""
