d1d6d8157abd8a2cc8243a7347a451d5dd10df7d
a-holm <jaholm.consulting@gmail.com>
2017-08-27 09:53:54 +0200

    Finish regular Mean Shift

--- meanShift/regularMeanShift.py
+++ meanShift/regularMeanShift.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Mean Shift unsupervised classification for machine learning.
+"""Mean Shift unsupervised hierarchical classification for machine learning.
 
 Mean Shift is very similar to the K-Means algorithm, except for one very
 important factor: you do not need to specify the number of groups prior to
@@ -30,3 +30,33 @@ Example:
 Todo:
     *
 """
+import numpy as np
+from sklearn.cluster import MeanShift
+from sklearn.datasets.samples_generator import make_blobs
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import style
+style.use("ggplot")
+
+# Create training data
+centers = [[1, 1, 1], [5, 5, 5], [3, 10, 10]]
+X, _ = make_blobs(n_samples=100, centers=centers, cluster_std=1)
+
+# Create Mean Shift object and fit data
+ms = MeanShift()
+ms.fit(X)
+labels = ms.labels_
+cluster_centers = ms.cluster_centers_
+print(cluster_centers)
+n_clusters_ = len(np.unique(labels))
+print("Number of estimated clusters: ", n_clusters_)
+
+# Plot
+colors = 10 * ['r', 'g', 'b', 'c', 'y', 'm']
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+for i in range(len(X)):
+    ax.scatter(X[i][0], X[i][1], X[i][2], c=colors[labels[i]], marker='o')
+ax.scatter(cluster_centers[:, 0], cluster_centers[:, 1], cluster_centers[:, 2],
+           marker="x", color='k', s=150, linewidths=5, zorder=10)
+plt.show()
