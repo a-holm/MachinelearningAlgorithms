f86cdb5391aef61d9aa81e28a7777d394b37410a
a-holm <jaholm.consulting@gmail.com>
2017-09-02 23:53:20 +0200

    Add Polynomial regression Python file

---
+++ Regression/PolynomialRegression/regularPolynomialRegression.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""Polynomial regression for machine learning.
+
+polynomial regression is a form of regression analysis in which the
+relationship between the independent variable x and the dependent variable y is
+modelled as an nth degree polynomial in x. Polynomial regression fits a
+nonlinear relationship between the value of x and the corresponding conditional
+mean of y, denoted E(y |x)Although polynomial regression fits a nonlinear model
+to the data, as a statistical estimation problem it is linear, in the sense
+that the regression function E(y | x) is linear in the unknown parameters that
+are estimated from the data. For this reason, polynomial regression is
+considered to be a special case of multiple linear regression.
+
+Example:
+
+        $ python regularPolynomialRegression.py
+
+Todo:
+    *
+"""
