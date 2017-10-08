c88dfc3a84bae591beaabe031f183ec8da29804d
a-holm <jaholm.consulting@gmail.com>
2017-10-08 15:10:46 +0200

    Fix regularKernelSVM.py

---
+++ Classification/KernelSVM/regularKernelSVM.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""Kernel SVM for machine learning.
+
+Kernel SVM is basically an SVM (see folder SupportVectorMachine ) which
+operates with kernels. Kernels use the inner product to help us transform the
+feature space to make it possible for Support Vector Machines to create a good
+hyperplane to effectively classify non-linear feature sets.
+
+Example:
+
+        $ python regularKernelSVM.py
+
+Todo:
+    *
